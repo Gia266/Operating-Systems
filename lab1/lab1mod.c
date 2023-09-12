639499550757f0a72f036c6f7b882eeaf1cc7f78
Valerie <20wyvs@queensu.ca>
2023-09-12 16:52:03 -0400

    Added code and functionalities required

--- lab1/lab1mod.c
+++ lab1/lab1mod.c
@@ -13,35 +13,83 @@
 #endif
 
 static int lab1_show(struct seq_file *m, void *v) {
-  /* some code here */
+
+  
+  //output:
+  seq_printf(m, "Current Process PCB Information \n");
+  
+  seq_printf(m, "Name = %s \n", current->comm);
+
+  seq_printf(m, "PID = %ld \n", current->pid); 
+  
+  seq_printf(m,"PPID = %d \n", task_ppid_nr(current));
+  
+  if (current->state <= 0){
+  seq_printf(m, "State = Running  \n");
+  }else if (current->state <= 2){
+  seq_printf(m, "State = Waiting \n");
+  }else{
+  seq_printf(m, "State = Stopped \n");
+  }
+
+  seq_printf(m, "Real UID = %ld \n", current->cred->uid);
+  
+  seq_printf(m, "Effective UID = %ld \n",  current->cred->euid);
+ 
+  seq_printf(m, "Saved UID = %ld \n", current->cred->suid);
+  
+  seq_printf(m, "Real GID = %ld \n", current->cred->gid);
+  
+  seq_printf(m, "Effective GID = %ld \n", current->cred->egid);
+  
+  seq_printf(m, "Saved GID = %ld \n", current->cred->sgid);
+  
+
+  //current-> points to task_struct of current proccess
+  current->state;
+  //
   return 0;
 }
 
 static int lab1_open(struct inode *inode, struct  file *file) {
-  return single_open(file, /*some function here*/, NULL);
+  return single_open(file, lab1_show, NULL);
 }
 
 #ifdef HAVE_PROC_OPS
 static const struct proc_ops lab1_fops = {
-  /* operation mapping */
+  .proc_open = lab1_open,
+  .proc_read = seq_read,
+  .proc_lseek = seq_lseek,
+  .proc_release = single_release,
+
 };
 #else
 static const struct file_operations lab1_fops = {
-  /* operation mapping */
+  .owner = THIS_MODULE,
+  .open = lab1_open,
+  .read = seq_read,
+  .llseek = seq_lseek,
+  .release = single_release,
+
 };
 #endif
 
 static int __init lab1_init(void) {
-  /* create proc entry */
+
+  if( proc_create("lab1", 0, NULL, &lab1_fops) == NULL){
+    return "ENOMEM";
+  }
+  //create lab1 file
   printk(KERN_INFO "lab1mod in\n");
   return 0;
 }
 
 static void __exit lab1_exit(void) {
-  /* remove proc entry */
+   remove_proc_entry("lab1", NULL);
+   //remove lab1 file
   printk(KERN_INFO "lab1mod out\n");
 }
 
 MODULE_LICENSE("GPL");
-module_init(/*some function here*/);
-module_exit(/*some function here*/);
+module_init(lab1_init);
+module_exit(lab1_exit);
